e547b3cd702397b75c428df596574e95a8973e2e
jaumeferDAM <jaumefernandezacanmas@gmail.com>
2016-09-22 17:54:41 +0200

    La puñetera agenda ;::))

--- src/model/Pagina.java
+++ src/model/Pagina.java
@@ -39,11 +39,12 @@ public class Pagina {
     public int getDiaSetmana() {
         return diaSetmana;
     }
-
+   
     public ArrayList<Cita> getCites() {
         return cites;
     }
 
+    
     public void crearCita(int horaInici, int horaFi, String anotacio) {
         
         if(cites==null) {
@@ -56,16 +57,28 @@ public class Pagina {
     public void modificarCita(Cita citaAntiga, Cita citaNova) {
        
         int pos=cites.indexOf(citaAntiga);
-        cites.set(pos, citaNova);
+        //cites.set(pos, citaNova);
         
         cites.remove(citaAntiga);
         cites.add(citaNova);
+        //cites.add(new Cita(15,17,"M3"));
     }
 
     public ArrayList<Cita> cercarCita(int horaInici) {
-        ArrayList<Cita> cerca = null;
-
+        ArrayList<Cita> cerca = new ArrayList<>();
+        
+//        for(int i = 0; i < cites.size; i++) {
+            for (Cita c : cites)
+            if (c.getHoraInici() == horaInici) {
+               cerca.add(c);
+                }
+                   
         return cerca;
     }
-
+    public void esborrarCita(Cita c)  {
+        cites.remove(c);
+        
+    }
+            
+    
 }
